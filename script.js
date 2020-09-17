0cded8914bedd00798a653eb0b02446269eed260
Kwaku Asare <lilkwaku55@yahoo.com>
2020-09-17 02:53:58 -0400

    additions

--- script.js
+++ script.js
@@ -42,10 +42,45 @@ function currentLocation() {
             url: queryUV,
             method: "GET"
           }).then(function(response){
-            $("#uvi").text(response.value); 
+            $("#uvi").text("UV Index: " + response.value); 
           });
 
         })
+
+        // 5-Day forecast
+        $.ajax({
+            url: "https://api.openweathermap.org/data/2.5/onecall?lat=" + lati + "&lon=" + longi + "&exclude=minutely,hourly&appid=1d885c7bf9c1a3c6f39c0d44c2182d2f",
+            method: "GET"
+        }).then(function(response) {
+            //console.log(response.daily[2].wind.speed)
+            for (var i = 1; i < 5; i++) {
+                $("#forecast"+i+" #foredate").text(new Date(response.daily[i].dt * 1000).toLocaleDateString("en-US"));
+                $("#forecast"+i+" #foretemperature").text("Temeperature: " + Math.floor((response.daily[i].temp.max - 273.15) * 1.8 + 32));
+                $("#forecast"+i+" #forehumidity").text("Humidity: " + response.daily[i].humidity)
+                //$("#forecast"+i+" #foredesc").text(response.daily[i].weather[i].description);
+            }
+        })
+
     }
 }
 currentLocation();
+
+
+//search by city
+$("#search").on("click", function() {
+    var cityname = $("#searchinfo").val();
+
+})
+
+var city = [];
+$("#buttons-view").empty();
+for  (var i = 0; i < city.length; i++) {
+    var c = $("<button>");
+    c.addClass("city-btn btn btn-primary btn-sm");
+    c.attr("data-name", city[i]);
+    c.text(city[i]);
+    $("#buttons-view").append(c);
+}
+
+$(document).on("click", ".city-btn",)
+
