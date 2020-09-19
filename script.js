b51870412af6b0c75b8a67e3bb04bfe7287ac1ad
Kwaku Asare <lilkwaku55@yahoo.com>
2020-09-18 22:09:44 -0400

    third commit

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ getDay();
 
 var APIKey = "1d885c7bf9c1a3c6f39c0d44c2182d2f";
 var queryURL = "";
-//var queryUV ="";
+var queryUV ="";
 
 var city = document.getElementById("city");
 var longi = 0;
@@ -33,21 +33,21 @@ function currentLocation() {
         }).then(function(response){
             city.textContent = response.name;
             console.log(response.name);
-            $("#temperature").text("Temeperature: " + Math.floor((response.main.temp - 273.15) * 1.8 + 32));
-            $("#humidity").text("Humidity: " + response.main.humidity);
-            $("#wind").text("Wind: " + response.wind.speed);
+            $("#temperature").text("Temperature: " + Math.floor((response.main.temp - 273.15) * 1.8 + 32) + " F");
+            $("#humidity").text("Humidity: " + response.main.humidity + "%");
+            $("#wind").text("Wind: " + Math.floor(response.wind.speed * 2.237) + " mph");
             
         })
         $.ajax({
             url: queryUV,
             method: "GET"
           }).then(function(response){
-            $("#uvi").text("UV Index: " + response.value); 
+            $("#vally").text(response.value); 
           });
 
         })
 
-        // 5-Day forecast
+        // 4-Day forecast
         $.ajax({
             url: "https://api.openweathermap.org/data/2.5/onecall?lat=" + lati + "&lon=" + longi + "&exclude=minutely,hourly&appid=1d885c7bf9c1a3c6f39c0d44c2182d2f",
             method: "GET"
@@ -55,32 +55,138 @@ function currentLocation() {
             //console.log(response.daily[2].wind.speed)
             for (var i = 1; i < 5; i++) {
                 $("#forecast"+i+" #foredate").text(new Date(response.daily[i].dt * 1000).toLocaleDateString("en-US"));
-                $("#forecast"+i+" #foretemperature").text("Temeperature: " + Math.floor((response.daily[i].temp.max - 273.15) * 1.8 + 32));
-                $("#forecast"+i+" #forehumidity").text("Humidity: " + response.daily[i].humidity)
-                //$("#forecast"+i+" #foredesc").text(response.daily[i].weather[i].description);
+                $("#forecast"+i+" #foretemperature").text("Temeperature: " + Math.floor((response.daily[i].temp.max - 273.15) * 1.8 + 32) + "F");
+                $("#forecast"+i+" #forehumidity").text("Humidity: " + response.daily[i].humidity + "%");
+                $("#forecast"+i+" #foredesc").html('<img src="http://openweathermap.org/img/wn/' + response.daily[i].weather[0].icon + '@2x.png">');
             }
         })
 
+    } else {
+        city.innerHTML = "location unknown.";
     }
 }
 currentLocation();
 
 
 //search by city
-$("#search").on("click", function() {
+citySearch = [];
+$("#search").on("click", function(event) {
+    event.preventDefault();
     var cityname = $("#searchinfo").val();
+    console.log(cityname);
+
+    citySearch.push(cityname);
+    saveSearches();
+    displaySearches();
+
+    queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityname + "&appid=" + APIKey;
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        city.textcontent = response.name;
+          $("#temperature").text("Temperature: " + Math.floor((response.main.temp - 273.15) * 1.8 + 32) + "F");
+          $("#humidity").text("Humidity: " + response.main.humidity + "%");
+          $("#wind").text("Wind: " + Math.floor(response.wind.speed * 2.237) + "mph");
+
+        lati = response.coord.lat;
+        longi = response.coord.lon;
+        queryUV = "http://api.openweathermap.org/data/2.5/uvi?appid=" + APIKey +
+        "&lat=" + lati + "&lon=" + longi;
+    
+        $.ajax({
+        url: queryUV,
+        method: "GET"
+     }).then(function(response) {
+        $("#vally").text("UV Index: " + response.value); 
+     });
+     
+     $.ajax({
+        url: "https://api.openweathermap.org/data/2.5/onecall?lat=" + lati + "&lon=" + longi + "&exclude=minutely,hourly&appid=95d5fa275b00b66c86cd3920c0de76f3",
+        method: "GET"
+      }).then(function(response) {
+          for (var i = 1; i < 5; i++) {
+            $("#forecast"+i+" #foredate").text(new Date(response.daily[i].dt * 1000).toLocaleDateString("en-US"));
+            $("#forecast"+i+" #foretemperature").text("Temeperature: " + Math.floor((response.daily[i].temp.max - 273.15) * 1.8 + 32) + "F");
+            $("#forecast"+i+" #forehumidity").text("Humidity: " + response.daily[i].humidity + "%")
+          } 
+        })
+    
+    })
+
 
 })
+// search by city ends //
 
-var cityname = [];
+//previous search
+
+$(document).on("click", ".atag", function(){
+    var cityname = $(this).text();
+  
+    queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityname + "&appid=" + APIKey;
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response){
+        city.textContent = response.name;
+        $("#temperature").text("Temperature: " + Math.floor((response.main.temp - 273.15) * 1.8 + 32) + "F");
+        $("#humidity").text("Humidity: " + response.main.humidity + "%");
+        $("#wind").text("Wind: " + Math.floor(response.wind.speed * 2.237) + "mph");
+        lati = response.coord.lat;
+        longi = response.coord.lon;
+        queryUV = "http://api.openweathermap.org/data/2.5/uvi?appid=" + APIKey +
+        "&lat=" + lati + "&lon=" + longi;
+        
+        $.ajax({
+          url: queryUV,
+          method: "GET"
+        }).then(function(response){
+          $("#vally").text(response.value); 
+        });
+        
+        $.ajax({
+          url: "https://api.openweathermap.org/data/2.5/onecall?lat=" + lati + "&lon=" + longi + "&exclude=minutely,hourly&appid=95d5fa275b00b66c86cd3920c0de76f3",
+          method: "GET"
+        }).then(function(response){
+          for(var i = 1; i = 5; i++){
+            $("#forecast"+i+" #foredate").text(new Date(response.daily[i].dt * 1000).toLocaleDateString("en-US"));
+            $("#forecast"+i+" #foretemperature").text("Temeperature: " + Math.floor((response.daily[i].temp.max - 273.15) * 1.8 + 32) + "F");
+            $("#forecast"+i+" #forehumidity").text("Humidity: " + response.daily[i].humidity + "%")
+          } 
+        });
+    });
+  });
+// previous ends
+
+ 
+
+
+
+
+
+
+//var cityname = [];
+var citySearch = [];
+function displaySearches() {
 $("#buttons-view").empty();
-for  (var i = 0; i < city.length; i++) {
-    var c = $("<button>");
+for  (var i = 0; i < citySearch.length; i++) {
+    var c = $("<li>", "<button>");
     c.addClass("city-btn btn btn-primary btn-sm");
-    c.attr("data-name", city[i]);
-    c.text(city[i]);
+    c.attr("data-name", citySearch[i]);
+    c.text(citySearch[i]);
     $("#buttons-view").append(c);
+  }
 }
-
-$(document).on("click", ".city-btn",)
-
+//$(document).on("click", ".city-btn",)
+function saveSearches() {
+    localStorage.setItem("searches", JSON.stringify(citySearch));
+}
+function init() {
+    var savedSearch = JSON.parse(localStorage.getItem("searches"));
+    if (savedSearch !== null) {
+        citySearch = savedSearch;
+    }
+    displaySearches();
+}
+init();
